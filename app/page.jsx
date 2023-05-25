a4aab761c74d01b93e49cfd7e78427aec5e7d19a
Nerdkidchiki <chikimiracle2000@gmail.com>
2023-05-25 16:34:26 +0100

    created homebody component

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,19 @@
 import HomeHeader from "@/components/HomeHeader";
+import Image from "next/image";
 
 export default function Home() {
-  return <HomeHeader />;
+  return (
+    <>
+      <HomeHeader />
+      {/* Body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+          width={"300"}
+          height={"100"}
+          alt="google logo"
+        />
+      </div>
+    </>
+  );
 }
