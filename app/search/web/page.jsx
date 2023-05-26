4b024cecf69b7e7b221dd0d5d90dd72c8da7ea4e
Nerdkidchiki <chikimiracle2000@gmail.com>
2023-05-26 16:01:24 +0100

    added functionality to  fetch query data from google
    -created search engine on google cloud

--- app/search/web/page.jsx
+++ app/search/web/page.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const SerachWebPage = () => {
+const SearchWebPage = async ({ searchParams: { searchTerm } }) => {
+  // console.log(searchTerm);
+  // const response = await fetch(
+  //   `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CONTEXT_KEY}&q=${searchTerm}`
+  // );
+  // const data = await response.json();
+  // console.log(data);
   return <div>page</div>;
 };
 
-export default SerachWebPage;
+export default SearchWebPage;
